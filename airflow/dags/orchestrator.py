7d1182a45cff7b92586ce7618a1d2929697ce707
brandon <brandonabuga7@gmail.com>
2026-03-19 14:28:05 +0300

    Added dag to transform data with dbt

--- airflow/dags/orchestrator.py
+++ airflow/dags/orchestrator.py
@@ -2,6 +2,8 @@ from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.providers.standard.operators.python import PythonOperator
 import sys
+from airflow.providers.docker.operators.docker import DockerOperator
+from docker.types import Mount
 
 sys.path.append("/opt/airflow/utils")
 
@@ -16,7 +18,7 @@ default_args = {
 }
 
 dag = DAG(
-    dag_id = "weather-api-orchestrator",
+    dag_id = "weather-api-dbt-orchestrator",
     default_args = default_args,
     schedule = timedelta(minutes = 5)
 )
@@ -26,4 +28,23 @@ with dag:
         task_id = "ingest_data_task",
         python_callable= safe_main_callable
     )
-    #task2
+    task2 = DockerOperator(
+        task_id = "transform-data-task",
+        image = "ghcr.io/dbt-labs/dbt-postgres:1.9.latest",
+        command = "run",
+        working_dir = "/usr/app",
+        mounts = [
+            Mount(source='/home/brandon/repos/Weather-Data_Project/dbt/my_project',
+                  target='/usr/app',
+                  type='bind'),
+            Mount(source='/home/brandon/repos/Weather-Data_Project/dbt/profiles.yml',
+                  target='/root/.dbt/profiles.yml',
+                  type='bind')
+        ],
+        network_mode = 'weather-data_project_my-network',
+        docker_url = "unix://var/run/docker.sock",
+        auto_remove = "success"
+    )
+
+    task1 >> task2
+    
